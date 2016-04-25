904af891c8e55721d50f4cccb780c55f2e053dc6
Sergio Morales <yeyox_114@hotmail.com>
2016-04-24 21:15:30 -0500

    Cambio de estructura

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$("#header").load('component/header.html');
+$("#footer").load("component/footer.html");
+$("#contenedor").load("module/home.html");
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var pag = getParameterByName('pag');
+$("#contenedor").load("module/"+pag+".html");

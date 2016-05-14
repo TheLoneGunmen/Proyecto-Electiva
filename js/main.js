db2d5e43ae3f89b8365882080223b7a393778dca
Sergio Morales <yeyox_114@hotmail.com>
2016-05-14 16:48:37 -0500

    Se cambio de nuevo la estructura de la pagina, se realizo el slider y se agregaron nuevos diseños a las paginas

--- js/main.js
+++ js/main.js
@@ -1,16 +1,56 @@
-$("#header").load('component/header.html');
-$("#footer").load("component/footer.html");
-//$("#contenedor").load("module/home.html");
+//Script para el menú responsive
+(function(){
+            var x = document.getElementById('main-nav');
+            var menu = document.getElementById('menu');
+            var contador = 0;
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+            x.addEventListener('click', function () {
+            x.classList.toggle('mostrar');
+
+            if(contador == 0){
+                contador = 1;
+                menu.classList.add('menu-show');
+                menu.classList.remove('menu-hide');
+        }else{
+            contador = 0;
+            menu.classList.add('menu-hide');
+            menu.classList.remove('menu-show');
+        }
+        });
+    }
+)();
+
+// Slider
+var $slider = $('#slider');
+var $btnPrev = $('#btn-prev');
+var $btnNext = $('#btn-next');
+
+$('#slider section:last').insertBefore('#slider section:first');
+$slider.css('margin-left','-'+100+'%');
+
+
+function moveRight() {
+  $slider.animate({marginLeft : '-'+200+'%'},700,function() {
+    $('#slider section:first').insertAfter("#slider section:last");
+    $slider.css('margin-left','-'+100+'%');
+  });
 }
 
-var pag = getParameterByName('pag');
-$("#contenedor").load("module/"+pag+".html");
+function moveLeft(){
+  $slider.animate({marginLeft : 0},700,function() {
+    $('#slider section:last').insertBefore("#slider section:first");
+    $slider.css('margin-left','-'+100+'%');
+  });
+}
+
+$btnNext.on('click',moveRight);
+$btnPrev.on('click',moveLeft);
+
+
+function autoPlay(){
+  setInterval(function() {
+    moveRight();
+  },5000);
+}
+
+autoPlay();
